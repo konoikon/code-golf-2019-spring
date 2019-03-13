653e68f733b718b35930c6b6696d972b5b0d999a
Konstantinos Oikonomou <kons.oikonomou@gmail.com>
2019-03-12 20:52:20 -0500

    lol

--- solutions/prob2.py
+++ solutions/prob2.py
@@ -1,5 +1,1 @@
-# print(sum(c!=e for c,e in zip(str(bin(int(input())))[2:].zfill(31),str(bin(int(input())))[2:].zfill(31))))
-
-with open('/Users/kon.oikon/Desktop/golf/code-golf-2019-spring/inputs/2.txt','r') as f:
-    print(*[sum(c!=e for c,e in zip(str(bin(int(line.split()[0])))[2:].zfill(31),str(bin(int(line.split()[1])))[2:].zfill(31)))
-          for line in f.readlines()])
+print(sum(c!=e for c,e in zip(str(bin(int(input())))[2:].zfill(31),str(bin(int(input())))[2:].zfill(31))))
